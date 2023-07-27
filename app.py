2a5eaf4f6287eed14df0697c9b85cf66a66e8c68
afterm00n <62357878+afterm00n@users.noreply.github.com>
2023-07-28 00:44:34 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,12 +3,12 @@ import requests
 
 app = Flask(__name__)
 
-@app.route('/check_website/world.al')
+@app.route('/check_website/<url>')
 def check_website(url):
     try:
         response = requests.get(url)
         status_code = response.status_code
-        return f"The status code of {url} is {status_code}"
+        return render_template('result.html', status_code=status_code)
     except requests.exceptions.RequestException as e:
         return f"Error: {e}"
 
